6170f97cebec4f350af8814025255f36d1e636e3
daniellerosa <danielle.rosa@scicrop.com>
2017-09-29 10:16:39 -0300

    Update ex 012909 dcr.cpp

--- ex 012909 dcr.cpp
+++ ex 012909 dcr.cpp
@@ -5,19 +5,19 @@ struct TNo {
        int N;
        
        TNo *prox;
-};                                              // se eu definisse o *P aqui, entre a chave e o ponto e vírgula, seria uma variável global
-                                                // *P é o endereço de memória apontado por P
+};                                              // se eu definisse o *P aqui, entre a chave e o ponto e vÃ­rgula, seria uma variÃ¡vel global
+                                                // *P Ã© o endereÃ§o de memÃ³ria apontado por P
 int main(){
-    TNo *P;                                   // esse *P é definido aqui pois eu quero que seja uma variável local. E como não é vetor, estamos usando *P
+    TNo *P;                                   // esse *P Ã© definido aqui pois eu quero que seja uma variÃ¡vel local. E como nÃ£o Ã© vetor, estamos usando *P
     int cont;
     
     printf("Conteudo de P=%d\n\n", P);
     P = (TNo *)malloc(sizeof(TNo));
     printf("Conteudo de P apos malloc = %d\n\n", P);
-    P-> N = 37;                                            // a seta significa que P está apontando para um campo N
-    P-> prox = NULL;                                      //Sintaxe: (*P).N  OU P -> N   é a mesma coisa
+    P-> N = 37;                                            // a seta significa que P estÃ¡ apontando para um campo N
+    P-> prox = NULL;                                      // Sintaxe: (*P).N  OU P -> N   Ã© a mesma coisa
     printf("Valor do no 1: %d", P-> N);          
-                                                          //quem tem o .N é quem está sendo apontado por P
+                                                          // quem tem o .N Ã© quem estÃ¡ sendo apontado por P
     P-> prox = (TNo *)malloc(sizeof(TNo)); 
     P-> prox -> N = 59;
     P-> prox -> prox = NULL;
@@ -28,9 +28,11 @@ int main(){
     P-> prox -> prox -> prox = NULL;
     printf("\nValor do no 3: %d\n\n", P -> prox -> prox -> N);
     
+    //ExibeLista(P);
+       
     printf("\nExibindo valores do while\n");
     cont = 1;
-    while (P !=NULL) {
+    while (P != NULL) {
           printf("\nValor do no %d: %d", cont, P -> N);
           P = P -> prox;
           cont++;
@@ -41,4 +43,16 @@ int main(){
     return 0;
 }
     
-       
+
+//void ExibeLista(TNo *Prim){
+     //int cont;
+     //printf("\nExibindo valores usando a funcao\n");
+     //cont = 1;
+     //while (Prim !=NULL) {
+          //printf("\nValor do no %d: %d", cont, Prim -> N);
+          //Prim = Prim -> prox;
+          //cont ++;
+          //}
+//}            
+
+
